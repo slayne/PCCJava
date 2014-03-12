a3939c2216646e70586bf91400aa341b487f306b
clement <clement@notnull>
2014-03-12 09:33:24 +0100

    Début main

--- PCCJava/src/aeroport/Main.java
+++ PCCJava/src/aeroport/Main.java
@@ -13,25 +13,36 @@ public class Main {
 		
 		// Variables .... 
 		String Favions = "C:/Users/clement.notnull/Google Drive/L3_Miage/Semestre 6/[Java] Projet PCC/ProjetJava-FI-2014/DonneesProjet/avions14-v1.txt";
-		
-		HashMap<String, Avion> tousLesAvions = null; //HashMap de tous les avions
-		
-		
-		
-		
-		//Création des agents
+		String FagentsMitemps = "C:/Users/clement.notnull/Google Drive/L3_Miage/Semestre 6/[Java] Projet PCC/ProjetJava-FI-2014/DonneesProjet/AgentsMiTemps-14-v1.txt";
+		String FagentsTempsPlein = "C:/Users/clement.notnull/Google Drive/L3_Miage/Semestre 6/[Java] Projet PCC/ProjetJava-FI-2014/DonneesProjet/AgentsTempsPlein-14-v1.txt";
+		String FvolA = "C:/Users/clement.notnull/Google Drive/L3_Miage/Semestre 6/[Java] Projet PCC/ProjetJava-FI-2014/DonneesProjet/ProgrammeVolsArrivees14-v2.txt";
+		String FvolB = "C:/Users/clement.notnull/Google Drive/L3_Miage/Semestre 6/[Java] Projet PCC/ProjetJava-FI-2014/DonneesProjet/ProgrammeVolsDeparts14-v2.txt";
 		
 		
-		// Création des vols
+		HashMap<String, Avion> tousLesAvions = new HashMap<String, Avion>(); //HashMap de tous les avions
+		HashMap<String, Agent> tousLesAgents = new HashMap<String, Agent>(); //HashMap de tous les agents
+		
+		
+		
 		try {
+			// Création des vols
 			Avion.creerTouslesAvions(Favions);
+			tousLesAvions = Avion.getLesAvions();
+			System.out.println("Nombre d'avions : " +tousLesAvions.size());
+			
+			//Création des agents
+			Agent.creerTouslesAgents(FagentsMitemps, FagentsTempsPlein);
+			tousLesAgents = Avion.getLesAgents();
+			System.out.println("Nombre d'agents : " +tousLesAgents.size());
+			
+			
+			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		Avion.getLesAvions();
 		
-		System.out.println(tousLesAvions.size());
+
 		
 		
 		//Affectation des taches
